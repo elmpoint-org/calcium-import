f2734c6a087fb839616d6462d87a13858791b6ae
hingobway <hingobway@users.noreply.github.com>
2025-08-11 10:40:11 -0400

    import

---
+++ src/index.ts
@@ -0,0 +1,262 @@
+import { readFile, writeFile } from 'node:fs/promises';
+
+import ical from 'ical.js';
+import { z } from 'zod';
+
+import dayjs, { Dayjs } from 'dayjs';
+import dayjsObj from 'dayjs/plugin/objectSupport';
+import dayjsUTC from 'dayjs/plugin/utc';
+dayjs.extend(dayjsObj);
+dayjs.extend(dayjsUTC);
+
+import { validate } from './validate';
+import path from 'node:path';
+
+// const n = 783;
+
+function prettyPrint(obj: unknown) {
+  console.log(JSON.stringify(obj, undefined, 2));
+}
+
+const cabinMap = {
+  Foster: '45617389-7678-42bf-bfb4-83304a389dc7',
+  House: '430a512f-6a35-4ed0-9e4b-4efa7a90a0e7',
+  Ide: 'd0b162c0-8a64-4946-b148-a7362c296709',
+  Kendrew: '2bb998a2-7c9a-4367-bcdb-b8e8a144b8a7',
+  Meeting: null,
+  Mosher: '649d65b6-9a50-491f-b0f2-8a0e22ee6275',
+};
+const cabins = Object.keys(cabinMap) as (keyof typeof cabinMap)[];
+const cabinShape = z.union([
+  z.never(),
+  z.never(),
+  ...cabins.map((c) => z.literal(c)),
+]);
+
+const dateShape = z.object({
+  isDate: z.literal(true),
+  year: z.number(),
+  month: z.number(),
+  day: z.number(),
+});
+const shape = z.object({
+  summary: z.string(),
+  dtstart: dateShape,
+  dtend: dateShape.optional(),
+  categories: cabinShape.optional(),
+  description: z.string().optional(),
+  rrule: z
+    .object({
+      freq: z.literal('DAILY'),
+      until: dateShape,
+    })
+    .optional(),
+});
+
+function parseDate(obj: z.infer<typeof dateShape>) {
+  return dayjs({
+    year: obj.year,
+    month: obj.month - 1,
+    day: obj.day,
+  });
+}
+function formatDate(obj: Dayjs) {
+  return obj.format('YYYY-MM-DD');
+}
+function parseFormatDate(obj: z.infer<typeof dateShape>) {
+  return formatDate(parseDate(obj));
+}
+
+export type Events = {
+  title: string;
+  description: string | undefined;
+  cabin:
+    | 'Foster'
+    | 'House'
+    | 'Ide'
+    | 'Kendrew'
+    | 'Meeting'
+    | 'Mosher'
+    | undefined;
+  dates: {
+    start: string;
+    end: string;
+  };
+};
+
+async function ImportICS() {
+  const filepath = path.join(__dirname, '../assets/CalciumEvents.ics');
+
+  // FETCH CALCIUM
+  const datadown = await fetch(process.env.CALCIUM_URL!, {})
+    .then((resp) => resp.text())
+    .catch((e) => console.log(e));
+  if (!datadown) return;
+  await writeFile(filepath, datadown);
+
+  const data = (await readFile(filepath).catch(() => {}))?.toString();
+  if (!data) return;
+
+  // convert to object
+  const cc = new ical.Component(ical.parse(data) as any, undefined);
+  const events = cc.getAllSubcomponents('vevent');
+
+  // PARSE
+  let broken = [];
+  let unparsed = [];
+  const parsed = events
+    .map((c, i) => {
+      let obj: any;
+
+      try {
+        obj = c
+          .getAllProperties()
+          .reduce(
+            (o: any, it: any) => ({ ...o, [it.name]: it.getValues()[0] }),
+            {}
+          );
+      } catch (e) {
+        broken.push({
+          number: i,
+          object: Object.assign({}, c.getAllProperties()),
+          error: e,
+        });
+        return;
+      }
+
+      const { data, error } = validate(obj, shape);
+      if (error || !data) {
+        unparsed.push({
+          number: i,
+          object: Object.assign({}, obj),
+          error: error,
+        });
+        return;
+      }
+
+      return {
+        title: data.summary,
+        description: data.description,
+        cabin: (data.categories && cabinMap[data.categories]) ?? null,
+        dates: {
+          start: parseFormatDate(data.dtstart),
+          end: data.rrule
+            ? parseFormatDate(data.rrule.until)
+            : formatDate(parseDate(data.dtstart).add(1, 'day')),
+        },
+      };
+    })
+    .filter((it) => !!it);
+
+  // DISPLAY
+
+  console.log('BROKEN: ', broken.length);
+  // prettyPrint(broken.slice(0,50))
+
+  console.log('UNPARSED: ', unparsed.length);
+  // prettyPrint(unparsed.slice(0,1))
+
+  const rr = parsed.filter((c) => !c.dates.end);
+  console.log('NO END DAY:', rr.length);
+  // console.log(rr.slice(rr.length - 50));
+
+  console.log('PARSED: ', parsed.length);
+  // console.log(parsed.slice(parsed.length - 50));
+  // console.log(parsed[240]);
+
+  // write to file
+  return parsed;
+
+  // writeFile(
+  //   path.join(__dirname, '../assets/IMPORT.json'),
+  //   JSON.stringify({ events: parsed })
+  // );
+}
+
+//  --------------------------------------------------------------
+
+// uploadData();
+async function uploadData() {
+  const parsed = await ImportICS();
+  if (!parsed) return;
+
+  const functions = parsed.map((event) => async () => {
+    {
+      return fetch('https://api.elmpoint.xyz/one/gql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: 'Bearer ' + process.env.EPC_TOKEN,
+        },
+        body: JSON.stringify({
+          query: `
+      mutation StayCreate($title: String!, $description: String!, $authorId: ID!, $dateStart: Int!, $dateEnd: Int!, $reservations: [StayReservationInput!]!) {
+        stayCreate(title: $title, description: $description, authorId: $authorId, dateStart: $dateStart, dateEnd: $dateEnd, reservations: $reservations) {
+          id
+        }
+      }`,
+          variables: {
+            title: event.title,
+            description: event.description ?? '',
+            authorId: '3ede5331-ab2d-46b6-89b4-dd68d0d4e2a0',
+            dateStart: stringToTS(event.dates.start),
+            dateEnd: stringToTS(event.dates.end),
+            reservations: [
+              {
+                name: event.title,
+                roomId: event.cabin ?? undefined,
+                customText: !event.cabin?.length ? 'Custom Event' : undefined,
+              },
+              {
+                name: '—————————————',
+                customText: 'Imported from Calcium',
+              },
+            ],
+          },
+        }),
+      })
+        .then((resp) => resp.json())
+        .catch((err) => console.error(err));
+    }
+  });
+
+  for (const func of functions) {
+    const response = await func();
+    prettyPrint(response);
+  }
+
+  // prettyPrint(resps);
+}
+
+/** convert to date timestamp.
+ *
+ * **By default, `isInputNotUTC` is true**, meaning the date from your current timezone will be taken. set to `false` to take the date at UTC time. */
+function dateTS(d: Date | number, isInputNotUTC: boolean = true) {
+  const day = d instanceof Date ? dayjs(d) : dayjs.unix(d);
+  return day.utc(isInputNotUTC).startOf('date').unix();
+}
+
+function stringToTS(d: string) {
+  return dateTS(dayjs(d, 'YYYY-MM-DD').unix());
+}
+
+// Testing()
+async function Testing() {
+  const runners = Array(8)
+    .fill(0)
+    .map(
+      () => () =>
+        new Promise<void>((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, 5000);
+        })
+    );
+
+  for (const func of runners) {
+    await func();
+    console.log('done!');
+  }
+}
+
+console.log('-- EOF --');
